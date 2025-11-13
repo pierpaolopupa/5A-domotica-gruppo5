4a72ef52b67cffab0e8bb24bc2c4e1e524b08fd5
Nogaye <pierpaolo.pupa@gmail.com>
2025-11-13 09:36:50 +0100

    Bosso : Sensore contatto porta corretto

--- Client/SensoreContatto.java
+++ Client/SensoreContatto.java
@@ -3,30 +3,40 @@ import java.net.*;
 import org.json.JSONObject;
 import java.util.Random;
 
-public class SensoreTemperatura {
-    public static void main(String[] args) throws IOException {
+public class SensoreContattoPorta {
+    public static void main(String[] args) throws IOException 
+    {
         Socket socket = new Socket("localhost", 12345); 
         PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         Random rand = new Random();
 
         while (true) {
-
-            double temperaturaCasuale = rand.nextDouble() * 50.0;
-
+           
+            boolean statoContatto = rand.nextBoolean();
+         
             JSONObject json = new JSONObject();
-            json.put("id", rand.nextInt(1000));
-            json.put("tipo", "temperatura");
-            json.put("valore", temperaturaCasuale);
-    
+            json.put("id", rand.nextInt(1000)); 
+            json.put("tipo", "contatto");
+            json.put("contatto", statoContatto); 
+
+            
             out.println(json.toString());
 
+            
             String response = in.readLine();
             if (response != null) {
-                System.out.println("Server: "+response); // response
+                System.out.println("Server: " + response);
             }
 
-            try { Thread.sleep(5000); } catch (InterruptedException e) {}
+            
+            try 
+            {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) 
+            {
+                e.printStackTrace();
+            }
         }
     }
 }
