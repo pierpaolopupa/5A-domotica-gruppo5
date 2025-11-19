4baf30a0537c79639c38ab6172bf0d880153f108
Nogaye <pierpaolo.pupa@gmail.com>
2025-11-19 08:36:29 +0100

    Lleshi + Sandru: implementata logica di ClientHandler con parsing JSON e gestione allarmi per temperatura, contatto, movimento e fumo

--- Server/Server.java
+++ Server/Server.java
@@ -13,3 +13,49 @@ public class MultiServer {
         }
     }
 }
+
+class ClientHandler implements Runnable {
+    private Socket socket;
+
+    public ClientHandler(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run() {
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+            String line;
+            while ((line = in.readLine()) != null) {
+                JSONObject json = new JSONObject(line); // Parsing JSON
+                String tipo = json.getString("tipoSensore");
+
+                if (tipo.equals("temperatura")) {
+                    int temp = json.getInt("temperatura");
+                    if (temp >= 40) {
+                        out.println("ALLARME: Temperatura troppo alta!");
+                    }
+                } else if (tipo.equals("contatto")) {
+                    boolean contatto = json.getBoolean("contatto");
+                    if (!contatto) {
+                        out.println("ALLARME: Porta aperta!");
+                    }
+                } else if (tipo.equals("movimento")) {
+                    boolean movimento = json.getBoolean("movimento");
+                    if (movimento) {
+                        out.println("ALLARME: Movimento rilevato!");
+                    }
+                } else if (tipo.equals("fumo")) {
+                    boolean fumo = json.getBoolean("fumo");
+                    if (fumo) {
+                        out.println("ALLARME: Fumo rilevato!");
+                    }
+                }
+            }
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
